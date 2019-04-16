023ea5cd68ec09c97939efd0694d019c2429bbf1
Benjamin <benjamin_verhage@yahoo.com>
2019-04-16 14:29:37 +0200

    v1

--- assets/my-counter.js
+++ assets/my-counter.js
@@ -11,26 +11,48 @@ $('#button-increase').on(
         //Deja je vais lire la valeur du champs siais (input)
         const inputField = $('#quantity');
         console.log('Nouvelle valeur attendue 1:' + inputField.val());
+
         //Recuperer la valeur max autorisee : data-max=n// .attri recup donne de lattribut objet
         const maxVal = parseInt(inputField.attr('data-max'));
+
         //Introduire la contrainte fonctionnelle: newVal < maxVal... sinon stop
         if (inputField.val() < maxVal) {
             //Incrementer la valeur de 1
             let newVal = parseInt(inputField.val()) + 1;
             //Afficher la nouvelle valeur
             inputField.val(newVal);
-
-            //TODO: ne pas oublier dactiver le bouton
         }
-        if (inputField.val() >= maxVal) {
+        if (inputField.val() == maxVal) {
             //Afficher le message maxi atteint
-            alert("10 est le maximum possible!")
+            alert("10 est le maximum possible!");
             const deactivate = $('#button-increase');
             $('#button-increase').attr('disabled', 'disabled');
-
-
         }
-        //TODO: afficher un message si maximum atteint
-        //TODO: desactiver le bouton + si maxi atteint
+    }
+);
+
+/** Gestionnaire d'evenement sur le bouton - (id=decrease-qty) */
+$('#button-decrease').on(
+    "click", function (event) {
+        //Deja je vais lire la valeur du champs siais (input)
+        const inputField = $('#quantity');
+        console.log('Nouvelle valeur attendue 1:' + inputField.val());
+
+        //Recuperer la valeur min autorisee : data-max=n// .attri recup donne de lattribut objet
+        const minVal = parseInt(inputField.attr('data-min'));
+
+        //Introduire la contrainte fonctionnelle: newVal < maxVal... sinon stop
+        if (inputField.val() > minVal) {
+            //Incrementer la valeur de -1
+            let newVal = parseInt(inputField.val()) - 1;
+            //Afficher la nouvelle valeur
+            inputField.val(newVal);
+        }
+        if (inputField.val() == minVal) {
+            //Afficher le message mini atteint
+            alert("1 est le min possible!");
+            const deactivate = $('#button-decrease');
+            $('#button-decrease').attr('disabled', 'disabled'); //affect an attribute and disable it
+        }
     }
 );
