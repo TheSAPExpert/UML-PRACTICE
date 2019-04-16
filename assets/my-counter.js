6a2d3969f0eb67b02ff7540a822ccde08ec20105
Benjamin <benjamin_verhage@yahoo.com>
2019-04-16 12:43:33 +0200

    added increase increment + desactivation@max + message

--- assets/my-counter.js
+++ assets/my-counter.js
@@ -5,84 +5,32 @@
 * @description Gere in composant de type compteur
 **/
 
-<script>
-    $('h1').html("hellojQuerry");
-
-//const monH1 = document.querySelector('h1');
-//monH1.innerHTML = 'Hello jQuery natif JS';
-
-//Deactivate previous click
-$('.company').on( //.company fait appel a tous les class qui on ce nom
-    "click",
-    function (event) {
-        const theOne = $(this);//l'objet clique
-let removeClass = true;// Par defaut, j'estime qu'il faut retirer la classe
-
-        if (theOne.hasClass('selected')) {
-        removeClass = false;
-    $(this).children('ul').hide();
-}
-
-//Supprime pour tout le monde la classe .selected
-$('.company').removeClass('selected');
-$('.company ul').hide();
-
-
-//Appliqur sur lelement ce qui doit etre fait
-        if (removeClass) {
-        $(this).addClass('selected');
-    $(this).children('ul').show();
-        } else {
-        $(this).removeClass('selected');
-    $(this).children('ul').hide();
-}
-}
-);
-
-//Challenge #1 detecter un click sur le bouton + de l'interface: complete
-//Challenge #2 recuperer la valeur du span.value: complete
-//Challenge #3 Ajouter 1 a la valeur courance du span value : complete
-//Challenge #4 Remplacer le contenu de span.value par la nouvelle valeur
-
-$('.up').on(
+/** Gestionnaire d'evenement sur le bouton + (id=increase-qty) */
+$('#button-increase').on(
     "click", function (event) {
-        console.info("Click sur + détecté!");
-    //#1,2 :objet qui contient la valeur a incrementer
-    const value = $('.value');
-    console.log('Valeur courante de span class==value:' + value.html());
+        //Deja je vais lire la valeur du champs siais (input)
+        const inputField = $('#quantity');
+        console.log('Nouvelle valeur attendue 1:' + inputField.val());
+        //Recuperer la valeur max autorisee : data-max=n// .attri recup donne de lattribut objet
+        const maxVal = parseInt(inputField.attr('data-max'));
+        //Introduire la contrainte fonctionnelle: newVal < maxVal... sinon stop
+        if (inputField.val() < maxVal) {
+            //Incrementer la valeur de 1
+            let newVal = parseInt(inputField.val()) + 1;
+            //Afficher la nouvelle valeur
+            inputField.val(newVal);
 
-    //#3 Ajouter 1 a la valeur courante
-    let nextValue = parseInt(value.html());
-        if (nextValue < 10) {
-        nextValue = nextValue + 1;
+            //TODO: ne pas oublier dactiver le bouton
+        }
+        if (inputField.val() >= maxVal) {
+            //Afficher le message maxi atteint
+            alert("10 est le maximum possible!")
+            const deactivate = $('#button-increase');
+            $('#button-increase').attr('disabled', 'disabled');
 
-    } else {
-        nextValue = 10
+
+        }
+        //TODO: afficher un message si maximum atteint
+        //TODO: desactiver le bouton + si maxi atteint
     }
-    console.log('Apres incrementation la valeur devient :' + nextValue);
-
-    //#4 Remplacer la nouvelle valeur sur le html
-    value.html(nextValue);
-}
 );
-$('.down').on(
-    "click", function (event) {
-        console.info("Click sur + détecté!");
-    //#1,2 objet qui contient la valeur a incrementer
-    const value = $('.value');
-    console.log('Valeur courante de span class==value:' + value.html());
-
-    //#3 Ajouter 1 a la valeur courante
-    let nextValue = parseInt(value.html());
-        if (nextValue > 1) {
-        nextValue = nextValue - 1;
-    } else {
-        nextValue = 1
-    }
-    console.log('Apres incrementation la valeur devient :' + nextValue);
-
-    //#4 Remplacer la nouvelle valeur sur le html
-    value.html(nextValue);
-}
-);
-</script>
